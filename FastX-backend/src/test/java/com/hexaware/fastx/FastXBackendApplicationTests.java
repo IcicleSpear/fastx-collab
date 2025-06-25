43ef1a86c33b27ef4f46362c1da1d76ec7a9f9d9
IcicleSpear <abhishek.ag2004@gmail.com>
2025-06-25 21:17:57 +0530

    Final Commit

---
+++ FastX-backend/src/test/java/com/hexaware/fastx/FastXBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexaware.fastx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastXBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
